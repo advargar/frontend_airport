e11f0f5f9949385d430792cf9d92dba0c4b1cb8b
Adrian Vargas <adrian.vargar22@gmail.com>
2021-11-22 08:47:22 -0600

    Pilot, Flight edit ready! FlightCatalog edit problem with api

--- src/app/components/flight/flight-edit/flight-edit.component.ts
+++ src/app/components/flight/flight-edit/flight-edit.component.ts
@@ -1,15 +1,85 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FlightService } from '../../../services/flight.service';
+import { Flight } from '../../../models/flight';
+import { UserService } from '../../../services/user.service';
+import { NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-flight-edit',
   templateUrl: './flight-edit.component.html',
-  styleUrls: ['./flight-edit.component.css']
+  styleUrls: ['./flight-edit.component.css'],
+  providers: [FlightService, UserService]
 })
 export class FlightEditComponent implements OnInit {
+  public flight: Flight;
+  public flights: any;
+  public status;
+  public token;
+  public minDate;
+  dateSelected;
 
-  constructor() { }
+	date: { year: number; month: number; } | undefined;
 
-  ngOnInit(): void {
+  constructor(
+    private _router: Router,
+    private _route: ActivatedRoute,
+    public _flightService: FlightService,
+    public _UserService: UserService,
+    private calendar: NgbCalendar,
+  ) {
+    this.flight = new Flight("", " "," ", " ", "", "", "", "");
+    this.token = this._UserService.getToken();
+    this.minDate = this.calendar.getToday();
   }
 
+  ngOnInit(): void {
+    const id = this._route.snapshot.params['id'];// capturamos el id que viene en la URL
+    this.getFlight(id);
+  }
+
+  getFlight(id) {
+    // Peticion ajax para sacar los datos del Avion
+    this._flightService.getFlight(id).subscribe(
+      response => {
+        if (response.status == 'success') {
+          this.flights = response.data;
+
+          this.flight = new Flight(
+            this.flight.id = this.flights.id,
+            this.flight.departure = this.flights.departure,
+            this.flight.arrival = this.flights.arrival,
+            this.flight.assignedDate = this.flights.assignedDate,
+            this.flight.passengers = this.flights.passengers,
+            this.flight.pilot = this.flights.pilot,
+            this.flight.coPilot = this.flights.coPilot,
+            this.flight.airplane = this.flights.airplane,
+          );
+        }
+      },
+      error => {
+        console.log(error);
+        this._router.navigate(['/gestionar-vuelo']);
+      }
+    );
+}
+
+onSubmit(form){
+      this.flight.assignedDate = this.dateSelected.year + "-" + this.dateSelected.month + "-" + this.dateSelected.day;
+      this._flightService.update(this.token,this.flight, this.flight.id).subscribe(
+        response =>{
+          if(response.status == 'success'){
+            this.flight = response.data;
+            this.status = response.status;
+            this._router.navigate(['/gestionar-vuelo']);
+          }else{
+            this.status = 'error';
+          }
+        },
+        error => {
+          this.status = 'error';
+          console.log(<any>error);
+        }
+      );
+    }
 }
--- src/app/components/pilot/pilot-edit/pilot-edit.component.ts
+++ src/app/components/pilot/pilot-edit/pilot-edit.component.ts
@@ -1,15 +1,73 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { PilotService } from '../../../services/pilot.service';
+import { Pilot } from '../../../models/pilot';
+import { UserService } from '../../../services/user.service';
 
 @Component({
   selector: 'app-pilot-edit',
   templateUrl: './pilot-edit.component.html',
-  styleUrls: ['./pilot-edit.component.css']
+  styleUrls: ['./pilot-edit.component.css'],
+  providers: [PilotService, UserService]
 })
 export class PilotEditComponent implements OnInit {
+  public pilot: Pilot;
+  public pilots: any;
+  public status;
+  public token;
+  constructor(
+    private _router: Router,
+    private _route: ActivatedRoute,
+    public _pilotService: PilotService,
+    public _UserService: UserService
+  ) {
+    this.pilot = new Pilot("", " ");
+    this.token = this._UserService.getToken();
+  }
 
-  constructor() { }
 
   ngOnInit(): void {
+    const id = this._route.snapshot.params['id'];// capturamos el id que viene en la URL
+    this.getPilot(id);
   }
+
+  getPilot(id) {
+    // Peticion ajax para sacar los datos del Avion
+    this._pilotService.getPilot(id).subscribe(
+      response => {
+        if (response.status == 'success') {
+          this.pilots = response.data;
+
+          this.pilot = new Pilot(
+            this.pilot.id = this.pilots.id,
+            this.pilot.idEmployee = this.pilots.idEmployee,
+          );
+          console.log(this.pilot);
+        }
+      },
+      error => {
+        console.log(error);
+        this._router.navigate(['/inicio']);
+      }
+    );
+}
+
+onSubmit(form){
+  this._pilotService.update(this.token,this.pilot, this.pilot.id).subscribe(
+    response =>{
+      if(response.status == 'success'){
+        this.pilot = response.data;
+        this.status = response.status;
+        this._router.navigate(['/inicio']);
+      }else{
+        this.status = 'error';
+      }
+    },
+    error => {
+      this.status = 'error';
+      console.log(<any>error);
+    }
+  );
+}
 
 }
